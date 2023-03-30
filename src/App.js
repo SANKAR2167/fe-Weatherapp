91b4b75ec0fbeb61f80b9a872ef7ad4c7f322992
SANKAR2167 <snakarkulandaivel@gmail.com>
2023-03-30 10:20:13 +0530

    weather app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Weather from './components/currentLocation';
+
+
+function App() {
+  return (
+      <div className="container">
+        <Weather />
+      </div>
+  );
+}
+
+export default App;
