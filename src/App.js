d957557915f798f4aac077d796f33b4693e6b761
SANKAR2167 <snakarkulandaivel@gmail.com>
2023-04-07 10:44:28 +0530

    auth implemented

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
+import { Navigate, Route, Routes } from 'react-router-dom';
 import './App.css';
-import Weather from './components/currentLocation';
+import Login from './components/login';
+import { Home } from './components/Home';
+import SignUp from './components/SignUp';
+
 
 
 function App() {
   return (
-      <div className="container">
-        <Weather />
-      </div>
+    <div>
+      <Routes>
+        <Route path='/' element={<ProductedRoute><Home /></ProductedRoute>} />
+        <Route path='/users/login' element={<Login />} />
+        <Route path='/users/signup' element={<SignUp/>} />
+      </Routes>
+    </div>
   );
+}
+
+function ProductedRoute({ children }) {
+  const isAuth = localStorage.getItem("token");
+  // console.log(isAuth);
+  return isAuth ? children : <Navigate replace to={"/users/login"} />;
 }
 
 export default App;
---
+++ src/components/Home.js
@@ -0,0 +1,9 @@
+import Weather from './currentLocation';
+
+export function Home() {
+  return (
+    <div className="container">
+      <Weather />
+    </div>
+  );
+}
